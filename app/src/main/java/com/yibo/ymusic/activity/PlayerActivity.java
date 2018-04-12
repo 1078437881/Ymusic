f4e0805e690a573193c620669b5afee99e24b0e5
1078437881 <1078437881@qq.com>
2018-04-12 22:09:20 +0800

    Initial commit

---
+++ app/src/main/java/com/yibo/ymusic/activity/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.yibo.ymusic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yibo.ymusic.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
